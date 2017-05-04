dbb66ff4a627574b8ff5bb247fd4e9c1e0dbe35d
miti01 <miti.m@freemail.hu>
2017-05-04 23:05:24 +0200

    Import Mockito

--- coins-android/app/src/test/java/EmailValidatorTestTest.java
+++ coins-android/app/src/test/java/EmailValidatorTestTest.java
@@ -1,11 +1,18 @@
+import com.nothingmagical.coins.Preferences;
+
 import static org.junit.Assert.*;
 import org.junit.Test;
+import static org.mockito.Mockito.*;
+
 /**
  * Created by MiTI on 2017. 05. 04..
  */
 public class EmailValidatorTestTest {
+
     @Test
     public void emailValidator_CorrectEmailSimple_ReturnsTrue() {
+
+
         assertTrue(true);
     }
 }
